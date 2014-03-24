a1c49cabaebf33982dd69a9191f87043743cb292
Gavin Noronha <gavinln@hotmail.com>
2014-03-23 19:07:29 -0700

    Added angular dependencies

--- js/app/scripts/app.js
+++ js/app/scripts/app.js
@@ -1,3 +1,34 @@
 'use strict';
 
-angular.module('angularVmApp', []);
+angular.module('angularVmApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/spring', {
+                templateUrl: 'views/spring.html',
+                controller: 'SpringCtrl'
+            })
+            .when('/summer', {
+                templateUrl: 'views/summer.html',
+                controller: 'SummerCtrl'
+            })
+            .when('/autumn', {
+                templateUrl: 'views/autumn.html',
+                controller: 'AutumnCtrl'
+            })
+            .when('/winter', {
+                templateUrl: 'views/winter.html',
+                controller: 'WinterCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
---
+++ js/app/scripts/controllers/seasons.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var angularVmApp = angular.module('angularVmApp');
+
+angularVmApp.controller('SpringCtrl', ['$scope', '$http', 'seasons',
+                       function ($scope, $http, seasons) {
+    $scope.season = seasons.state.spring;
+    $scope.seasons = seasons;
+    $http.get('http://www.omdbapi.com/?s=spring').success(
+        function(data) {
+            $scope.movies = data.Search;
+        }
+    );
+}]);
+
+angularVmApp.controller('SummerCtrl', ['$scope', '$http', 'seasons',
+                       function ($scope, $http, seasons) {
+    $scope.season = seasons.state.summer;
+    $scope.seasons = seasons;
+    $http.get('http://www.omdbapi.com/?s=summer').success(
+        function(data) {
+            $scope.movies = data.Search;
+        }
+    );
+}]);
+
+angularVmApp.controller('AutumnCtrl', ['$scope', '$http', 'seasons',
+                       function ($scope, $http, seasons) {
+    $scope.season = seasons.state.autumn;
+    $scope.seasons = seasons;
+    $http.get('http://www.omdbapi.com/?s=autumn').success(
+        function(data) {
+            $scope.movies = data.Search;
+        }
+    );
+}]);
+
+angularVmApp.controller('WinterCtrl', ['$scope', '$http', 'seasons',
+                       function ($scope, $http, seasons) {
+    $scope.season = seasons.state.winter;
+    $scope.seasons = seasons;
+    $http.get('http://www.omdbapi.com/?s=winter').success(
+        function(data) {
+            $scope.movies = data.Search;
+        }
+    );
+}]);
