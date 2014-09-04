8cc1aee1dcf388975a8be570c95f466b016536f8
Gavin Noronha <gnoronha@financialengines.com>
2014-09-03 17:06:30 -0700

    Fixed formatting

--- js/app/scripts/app.js
+++ js/app/scripts/app.js
@@ -1,38 +1,38 @@
 'use strict';
 
 angular.module('angularVmApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
-  'ui.router'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'ui.router'
 ])
 .config(function ($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/home');
-  $stateProvider
-  .state('home', {
-    url: '/home',
-    templateUrl: 'views/main.html',
-    controller: 'MainCtrl'
-  })
-  .state('home.spring', {
-    url: '/spring',
-    templateUrl: 'views/spring.html',
-    controller: 'SpringCtrl'
-  })
-  .state('home.summer', {
-    url: '/summer',
-    templateUrl: 'views/summer.html',
-    controller: 'SummerCtrl'
-  })
-  .state('home.autumn', {
-    url: '/autumn',
-    templateUrl: 'views/autumn.html',
-    controller: 'AutumnCtrl'
-  })
-  .state('home.winter', {
-    url: '/winter',
-    templateUrl: 'views/winter.html',
-    controller: 'WinterCtrl'
-  });
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+    .state('home', {
+        url: '/home',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+    })
+    .state('home.spring', {
+        url: '/spring',
+        templateUrl: 'views/spring.html',
+        controller: 'SpringCtrl'
+    })
+    .state('home.summer', {
+        url: '/summer',
+        templateUrl: 'views/summer.html',
+        controller: 'SummerCtrl'
+    })
+    .state('home.autumn', {
+        url: '/autumn',
+        templateUrl: 'views/autumn.html',
+        controller: 'AutumnCtrl'
+    })
+    .state('home.winter', {
+        url: '/winter',
+        templateUrl: 'views/winter.html',
+        controller: 'WinterCtrl'
+    });
 });
