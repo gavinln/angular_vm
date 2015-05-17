1ccf05d9372415173d03c103e82d01375133ee80
Gavin Noronha <gavinln@hotmail.com>
2015-05-16 22:36:07 -0700

    Changed to use bash login shell

--- ssh-vm.bat
+++ ssh-vm.bat
@@ -1,4 +1,6 @@
 @echo off
 
 set KEY_FILE=~/.ssh/id_rsa
-bash -c "eval `ssh-agent`; ssh-add; vagrant ssh vm -- -t '. /etc/profile; cd /srv/share; bash'"
+bash -c "eval `ssh-agent`; ssh-add; vagrant ssh vm -- -t 'cd /srv/share; bash -l'"
+
+REM bash -c "eval `ssh-agent`; ssh-add; vagrant ssh vm -- -t '. /etc/profile; cd /srv/share; bash'"
