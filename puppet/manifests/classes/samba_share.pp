47de3d2fd92c1eef539ae2faca388a8146693ae3
Gavin Noronha <gavinln@hotmail.com>
2015-05-16 19:45:13 -0700

    updated samba share options

--- puppet/manifests/classes/samba_share.pp
+++ puppet/manifests/classes/samba_share.pp
@@ -9,8 +9,9 @@ class samba_share {
     class {'::samba::server':
         workgroup            => 'WORKGROUP',
         server_string        => 'Example File Server 01',
-        netbios_name         => 'IPython',
+        netbios_name         => 'test',
         interfaces           => [ 'lo', 'eth0' ],
+        # comment out next line to allow all access to samba share
         hosts_allow          => [ '127.', '192.168.' ],
         local_master         => 'yes',
         map_to_guest         => 'Bad User',
@@ -19,6 +20,8 @@ class samba_share {
         extra_global_options => [
             'printing = BSD',
             'printcap name = /dev/null',
+            'create mask = 0777',
+            'directory mask = 2777',
         ],
         shares => {
             'share' => [
@@ -27,7 +30,13 @@ class samba_share {
                 'browseable = yes',
                 'writable = yes',
                 'guest ok = yes',
+                'force user = vagrant',
+                'force group = vagrant',
                 'available = yes',
+# Need to add the following otherwise get the error on somem systems
+# "Oplock break failed for file" in /var/log/samba
+                'oplocks = False',
+                'level2 oplocks = False'
             ],
         },
         selinux_enable_home_dirs => true,
