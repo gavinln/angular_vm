2ec0bb6734102ec164852d82f88c01399f9879e2
Gavin Noronha <gavinln@hotmail.com>
2015-05-16 19:46:18 -0700

    changed from zsh to bash

--- ssh-vm.bat
+++ ssh-vm.bat
@@ -1,4 +1,4 @@
 @echo off
 
 set KEY_FILE=~/.ssh/id_rsa
-bash -c "eval `ssh-agent`; ssh-add; vagrant ssh vm -- -t '. /etc/profile; cd /srv/share; zsh'"
+bash -c "eval `ssh-agent`; ssh-add; vagrant ssh vm -- -t '. /etc/profile; cd /srv/share; bash'"
